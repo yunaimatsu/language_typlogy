e92888f15f53b251e9097e99f5432e2489c53388
yunaimatsu <114282747+yunaimatsu@users.noreply.github.com>
2023-11-18 10:59:08 +0900

    Cambio 3
    Cambio 3

--- nlp_tutorial.py
+++ nlp_tutorial.py
@@ -1,29 +1,5 @@
-class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self.balance = balance
+import nltk
+nltk.download('punkt')
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            print(f"Deposited {amount} dollars. New balance: {self.balance}")
-        else:
-            print("Invalid deposit amount. Please deposit a positive amount.")
-
-    def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            print(f"Withdrew {amount} dollars. New balance: {self.balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient funds.")
-
-    def get_balance(self):
-        return self.balance
-
-# インスタンスの作成
-account1 = BankAccount(owner="John Doe", balance=1000)
-
-# 操作例
-account1.deposit(500)
-account1.withdraw(200)
-print(f"Current balance: {account1.get_balance()} dollars")
+import nltk
+nltk.download('averaged_perceptron_tagger')
