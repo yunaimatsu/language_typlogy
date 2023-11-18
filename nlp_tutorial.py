e58be06daf17cc3dee9e12b607d95a88263091cf
yunaimatsu <114282747+yunaimatsu@users.noreply.github.com>
2023-11-18 10:36:49 +0900

    Estoy desarrollando.
    Muy divertido.

--- nlp_tutorial.py
+++ nlp_tutorial.py
@@ -1,10 +1,29 @@
-"""
-Este es un tutorial de procesamiento del lenguaje natural (NLP).
-Explica cómo usar algunas bibliotecas y técnicas comunes de NLP en Python.
-"""
-import nltk
-from nltk.tokenize import word_tokenize
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
 
-text = "This is a sample sentence."
-tokens = word_tokenize(text)
-print(tokens)
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited {amount} dollars. New balance: {self.balance}")
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount} dollars. New balance: {self.balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient funds.")
+
+    def get_balance(self):
+        return self.balance
+
+# インスタンスの作成
+account1 = BankAccount(owner="John Doe", balance=1000)
+
+# 操作例
+account1.deposit(500)
+account1.withdraw(200)
+print(f"Current balance: {account1.get_balance()} dollars")
